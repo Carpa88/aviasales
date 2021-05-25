4aa588de72b9d98dae42c1516d0bc7eeed25612e
Olga <o.i.morozova.14@gmail.com>
2021-05-25 11:18:33 +0300

    Project inited

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState }from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+import './components/adaptive.scss'
+
+function App(){
+  
+  return(
+    
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
